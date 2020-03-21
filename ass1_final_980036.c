40372e8627aa9ce3abb3e230fa9a0ea5ef856fbd
wenyee-ang <62456103+wenyee-ang@users.noreply.github.com>
2020-03-21 15:12:02 +1100

    Update ass1_final_980036.c

--- ass1_final_980036.c
+++ ass1_final_980036.c
@@ -1,44 +1,11 @@
-/* Solution to comp10002 Assignment 1, 2019 semester 2.
-
-   Authorship Declaration:
-
-   (1) I certify that the program contained in this submission is completely
-   my own individual work, except where explicitly noted by comments that
-   provide details otherwise.  I understand that work that has been developed
-   by another student, or by me in collaboration with other students,
-   or by non-students as a result of request, solicitation, or payment,
-   may not be submitted for assessment in this subject.  I understand that
-   submitting for assessment work developed by or in collaboration with
-   other students or non-students constitutes Academic Misconduct, and
-   may be penalized by mark deductions, or by other penalties determined
-   via the University of Melbourne Academic Honesty Policy, as described
-   at https://academicintegrity.unimelb.edu.au.
-
-   (2) I also certify that I have not provided a copy of this work in either
-   softcopy or hardcopy or any other form to any other student, and nor will
-   I do so until after the marks are released. I understand that providing
-   my work to other students, regardless of my intention or any undertakings
-   made to me by that other student, is also Academic Misconduct.
-
-   (3) I further understand that providing a copy of the assignment
-   specification to any form of code authoring or assignment tutoring
-   service, or drawing the attention of others to such services and code
-   that may have been made available via such a service, may be regarded
-   as Student General Misconduct (interfering with the teaching activities
-   of the University and/or inciting others to commit Academic Misconduct).
-   I understand that an allegation of Student General Misconduct may arise
-   regardless of whether or not I personally make use of such solutions
-   or sought benefit from such actions.
-
-   Signed by: WEN YEE ANG 980036
-   Dated:     13/09/2019
-
+/* comp10002 Assignment 1, 2019 semester 2. Text editor program.
+WEN YEE ANG
+Dated: 13/09/2019
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 
 #define DEBUG 0
 #define MAXCHAR 999 /*max number of characters per input line*/
@@ -51,7 +18,6 @@
 #define NUMSPACES 4
 #define NEXTLINE 1
 #define ENDOFFILE 0
-
 
 int mygetchar();
 int make_para(char *A, int j);
@@ -612,5 +578,3 @@ mygetchar() {
 	}
 	return c;
 }
-
-/*ALGORITHMS ARE FUN!!! :) */
